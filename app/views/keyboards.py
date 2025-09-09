3ed317cdda4606f5f2d292e3ae4ff2e93d45b8cc
Ali <alifbahrami13766@gmail.com>
2025-09-09 17:14:58 +0330

    fe:add  inline keyboard

--- app/views/keyboards.py
+++ app/views/keyboards.py
@@ -25,43 +25,36 @@ class ReplyKeyboard:
             for i in range(0, len(buttons), column):
                 self.markup.row(*buttons[i:i + column])
         
-    
+
+class InlineKeyboard:
+    def __init__(self, list_name_buttons: list = None, row_key: int = 0, column: int = 0):
+        self.row = row_key
+        self.column = column
+        self.markup = types.InlineKeyboardMarkup()
+        if list_name_buttons:
+            self.create_button(self.row, self.column, list_name_buttons)
+
+    def get_markup(self):
+        return self.markup
+
+    def create_button(self, row: int, column: int, list_name_buttons: list):
+        if column == 0 or row == 0:
+            # هر دکمه در یک ردیف
+            for text in list_name_buttons:
+                self.markup.add(types.InlineKeyboardButton(text, callback_data=text))
+        else:
+            # دکمه‌ها بر اساس تعداد ستون‌ها
+            buttons = [types.InlineKeyboardButton(text, callback_data=text) for text in list_name_buttons]
+            for i in range(0, len(buttons), column):
+                self.markup.row(*buttons[i:i + column])
+
+
+
 rep= ReplyKeyboard(list_name_buttons=["driver","student"])
+
+kb = InlineKeyboard(["ثبت نام ", "اطلاعات دانش اموزان ", "جزییات مالی ",], column=2)
+
 
 rep_mark= rep.get_markup()
 
-# class Keyboards:
-#     @staticmethod
-#     def main_menu_keyboard():
-#         """Reply Keyboard: نمایش منوی اصلی"""
-#         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#         markup.add(
-#             types.KeyboardButton("📋 لیست کارها"),
-#             types.KeyboardButton("ℹ️ درباره ما")
-#         )
-#         return markup
-
-#     @staticmethod
-#     def inline_actions():
-#         """Inline Keyboard: نمایش دکمه‌های اکشن"""
-#         markup = types.InlineKeyboardMarkup()
-#         btn1 = types.InlineKeyboardButton("🌐 وبسایت", url="https://example.com")
-#         btn2 = types.InlineKeyboardButton("➕ افزودن", callback_data="add_task")
-#         markup.add(btn1, btn2)
-#         return markup
-
-
-
-# # ✅ Reply Keyboard
-# def main_menu_keyboard():
-#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#     markup.add(types.KeyboardButton("📋 لیست کارها"), types.KeyboardButton("ℹ️ درباره ما"))
-#     return markup
-
-# # ✅ Inline Keyboard
-# def inline_actions():
-#     markup = types.InlineKeyboardMarkup()
-#     btn1 = types.InlineKeyboardButton("🌐 وبسایت", url="https://example.com")
-#     btn2 = types.InlineKeyboardButton("➕ افزودن", callback_data="add_task")
-#     markup.add(btn1, btn2)
-#     return markup
+ 
