cc108dd1ffd3b752800f4ac9f192e24f06c68f3b
Ali <alifbahrami13766@gmail.com>
2025-09-14 15:56:40 +0330

    fea:add method callback_handelers

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -3,10 +3,12 @@ from app.bot.connection_manager import create_bot_with_retry,run_bot_white_try
 from app.handlers.start import Start
 from app.core.logger import LogService
 from app.handlers.middleware import middleware_handeler
+from app.handlers.callback import callback_handelers
 def run():
     LogService._root.debug("Bot.bot.run.start")
     bot = create_bot_with_retry(middleware=True)
     middleware_handeler(bot)
+    callback_handelers(bot)
     Start(bot)
     run_bot_white_try(bot)
    
