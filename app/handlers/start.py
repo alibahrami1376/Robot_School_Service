0537cfcb735e912dfef051a56cccac816777b2fc
Ali <alifbahrami13766@gmail.com>
2025-09-14 13:29:54 +0330

    refactore : add inline keyboards

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,26 +1,17 @@
+from telebot import TeleBot
 from telebot.types import Message
-from app.views.keyboards import rep_mark
+from app.views.keyboards import main_mark
+from app.Text_messages.system import SystemMessages
 from app.core.logger import LogService
 
 data = {723644977:"Alibahrami" }
 
-def Start(bot):
+def Start(bot:TeleBot):
     LogService._root.debug("handelers.start.register.start")
 
     @bot.message_handler(commands=['start'])
     def handle_start(m: Message):
-
-        if m.chat.username is  None:
-            bot.send_message(m.chat.id,"please set usrname....")
-            return
-      
-        user = data.get(m.chat.id)
-
-        if user :
-            bot.send_message(m.chat.id,F"hi {user} ......")
-        else :
-            data[m.chat.id]=m.chat.username
-            bot.send_message(m.chat.id, "سلام! 👋 به بات خوش اومدی.", reply_markup=rep_mark)
+        bot.send_message(m.chat.id,SystemMessages.WELCOME_START,reply_markup=main_mark)
 
 
 
