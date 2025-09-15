199880345c3d576037b8f98ce5161f8e9391b9e4
alibahrami1376 <69414558+alibahrami1376@users.noreply.github.com>
2025-09-15 21:06:18 +0330

    fix(callback): use call.from_user.id instead of call.message.from_user.id to prevent KeyError

--- app/handlers/callback.py
+++ app/handlers/callback.py
@@ -3,7 +3,7 @@ from telebot import TeleBot
 from telebot.types import CallbackQuery
 from app.views.keyboards import driver_mark,student_mark
 from app.Text_messages.system import SystemMessages
-
+from app.core.session import session
 
 def callback_handelers(bot:TeleBot):
         
@@ -14,13 +14,19 @@ def callback_handelers(bot:TeleBot):
            
             case "Driver":
                 bot.delete_message(call.message.chat.id, call.message.message_id)
-                bot.send_message(call.message.chat.id,SystemMessages.CHOICE,reply_markup=driver_mark)
+                message= bot.send_message(call.message.chat.id,SystemMessages.CHOICE,reply_markup=driver_mark)
+                session.set_last_messageid(call.from_user.id,message.message_id)
+
             case "Student":
                 bot.delete_message(call.message.chat.id, call.message.message_id)
-                bot.send_message(call.message.chat.id,SystemMessages.CHOICE,reply_markup=student_mark)
+                message = bot.send_message(call.message.chat.id,SystemMessages.CHOICE,reply_markup=student_mark)
+                session.set_last_messageid(call.from_user.id,message.message_id)
+
             case "Contact_us":
                 bot.delete_message(call.message.chat.id, call.message.message_id)    
-                bot.send_message(call.message.chat.id,"Contact_us")
+                message= bot.send_message(call.message.chat.id,"Contact_us")
+                session.set_last_messageid(call.from_user.id,message.message_id)
+
             case "reg_ST":
                 pass
             case "info_ST":
