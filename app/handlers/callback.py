c480851f069f5a28c81a293ae9fb1684d44db120
alibahrami1376 <69414558+alibahrami1376@users.noreply.github.com>
2025-09-15 21:52:49 +0330

    feat(callback): add step handling and driver register flow
    - integrate session.set_step with Enum values (Driver, Student, Contact_us)
    - add DriverRegisterMessage for driver registration
    - handle reg_DR case with first name input

--- app/handlers/callback.py
+++ app/handlers/callback.py
@@ -3,7 +3,8 @@ from telebot import TeleBot
 from telebot.types import CallbackQuery
 from app.views.keyboards import driver_mark,student_mark
 from app.Text_messages.system import SystemMessages
-from app.core.session import session
+from app.Text_messages.dr_register import DriverRigesterMessage
+from app.core.session import session,step
 
 def callback_handelers(bot:TeleBot):
         
@@ -16,25 +17,31 @@ def callback_handelers(bot:TeleBot):
                 bot.delete_message(call.message.chat.id, call.message.message_id)
                 message= bot.send_message(call.message.chat.id,SystemMessages.CHOICE,reply_markup=driver_mark)
                 session.set_last_messageid(call.from_user.id,message.message_id)
-
+                session.set_step(call.from_user.id,step.DRIVER.value)
             case "Student":
                 bot.delete_message(call.message.chat.id, call.message.message_id)
                 message = bot.send_message(call.message.chat.id,SystemMessages.CHOICE,reply_markup=student_mark)
                 session.set_last_messageid(call.from_user.id,message.message_id)
+                session.set_step(call.from_user.id,step.STUDENT.value)
 
             case "Contact_us":
                 bot.delete_message(call.message.chat.id, call.message.message_id)    
                 message= bot.send_message(call.message.chat.id,"Contact_us")
                 session.set_last_messageid(call.from_user.id,message.message_id)
+                session.set_step(call.from_user.id,step.CONTACT_US.value)
 
             case "reg_ST":
                 pass
+
             case "info_ST":
                 pass
             case "Financial_ST":
                 pass
             case "reg_DR":
-                pass
+                bot.delete_message(call.message.chat.id, call.message.message_id)    
+                message= bot.send_message(call.message.chat.id,DriverRigesterMessage.INPUT_FIRST_NAME,)
+                session.set_last_messageid(call.from_user.id,message.message_id)
+                session.set_step(call.from_user.id,step.DRIVER_RG.value)
             case "info_DR":
                 pass
             case "Financial_DR":
