84086733c980b50615251d549ccb30ae32deb0a4
Ali <alifbahrami13766@gmail.com>
2025-09-16 12:01:56 +0330

    feat(bot): add back menu to driver registration and handlers for previous_stage & main_menu

--- app/handlers/callback.py
+++ app/handlers/callback.py
@@ -1,10 +1,12 @@
 
 from telebot import TeleBot
 from telebot.types import CallbackQuery
-from app.views.keyboards import driver_mark,student_mark
+from app.views.keyboards import driver_mark,student_mark,back_menu_mark,main_mark
 from app.Text_messages.system import SystemMessages
 from app.Text_messages.dr_register import DriverRigesterMessage
 from app.core.session import session,step
+
+ 
 
 def callback_handelers(bot:TeleBot):
         
@@ -39,11 +41,18 @@ def callback_handelers(bot:TeleBot):
                 pass
             case "reg_DR":
                 bot.delete_message(call.message.chat.id, call.message.message_id)    
-                message= bot.send_message(call.message.chat.id,DriverRigesterMessage.INPUT_FIRST_NAME,)
+                message= bot.send_message(call.message.chat.id,DriverRigesterMessage.INPUT_FIRST_NAME,reply_markup=back_menu_mark)
                 session.set_last_messageid(call.from_user.id,message.message_id)
-                session.set_step(call.from_user.id,step.DRIVER_RG.value)
+                session.set_step(call.from_user.id,step.DRIVER_RG_INPUT_NAME.value)
             case "info_DR":
                 pass
             case "Financial_DR":
-                pass    
+                pass 
+            case "previous_stage":
+                pass
+            case "main_menu":
+                bot.delete_message(call.message.chat.id, call.message.message_id)    
+                message= bot.send_message(call.message.chat.id,SystemMessages.WELCOME_START,reply_markup=main_mark)
+                session.set_last_messageid(call.from_user.id,message.message_id)
+                session.set_step(call.from_user.id,step.START.value)       
         
